7a31c3d2f8a6564cf3a309fd89c1b87f4c8f8795
SkiTuscany <steve.palmer@ericsson.com>
2021-01-15 16:17:54 +0000

    2 files added

---
+++ src/app/filmtable/filmtable.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import Character from './Character';
+@Component({
+   selector: 'app-root',
+   templateUrl: './app.component.html',
+   styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+   characters: Character[] = [
+    {
+    actor_name: 'Peter Dinklage',
+    character_name: 'Tyrion Lannister',
+    gender: 'Male',
+        status: 'Alive'
+    },
+    {
+    actor_name: 'Sean Bean',
+    character_name: 'Ned Stark',
+    gender: 'Male',
+    status: 'Dead'
+    },
+    {
+    actor_name: 'Emilia Clark',
+    character_name: 'Khaleesi',
+    gender: 'Female',
+    status: 'Alive'
+    },
+    {
+    actor_name: 'Catelyn Stark',
+    character_name: 'Michelle Fairley',
+    gender: 'Female',
+    status: 'Dead'
+    }
+  ];
+}
